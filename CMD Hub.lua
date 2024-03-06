e8fac1377c0241f4e41a97295f0183c03b645185
cooolchillX <158112913+cooolchillX@users.noreply.github.com>
2024-03-06 17:26:29 -0500

    Update CMD Hub.lua

--- CMD Hub.lua
+++ CMD Hub.lua
@@ -1,5 +1,166 @@
-coool = "scripts"
-chill = "coming"
-X = "soon..."
+local UI = Instance.new("ScreenGui")
+UI.Name = "CMD"
+UI.Parent = game.Players.LocalPlayer.PlayerGui
 
-print("More", coool, "will be", chill, X)
+i = true
+
+local base = Instance.new("Frame")
+base.Name = "Base"
+base.Size = UDim2.new(0.5, 0, 0.4, 0)
+base.Position = UDim2.new(0.1, 0, 0, 0)
+base.BackgroundColor3 = Color3.new(0, 0, 0)
+base.BorderSizePixel = 0
+base.BackgroundTransparency = 0.5
+base.Parent = UI
+
+local arrow = Instance.new("TextLabel")
+arrow.Name = "Arrow"
+arrow.Size = UDim2.new(0, 10, 0, 15)
+arrow.Position = UDim2.new(0.11, 0, 0.1, 0)
+arrow.Text = ">"
+arrow.TextColor3 = Color3.new(1, 1, 1)
+arrow.BackgroundTransparency = 1
+arrow.Parent = UI
+
+local label = Instance.new("TextLabel")
+label.Name = "Label"
+label.Size = UDim2.new(0, 400, 0, 20)
+label.Position = UDim2.new(0.1, 0, 0.01, 0)
+label.Text = "Welcome To The Command Prompt"
+label.TextColor3 = Color3.new(1, 1, 1)
+label.BackgroundTransparency = 1
+label.Parent = UI
+label.TextSize = 18
+
+local ret = Instance.new("TextLabel")
+ret.Name = "Return"
+ret.Size = UDim2.new(0, 270, 0, 20)
+ret.Position = UDim2.new(0.1, 0, 0.35, 0)
+ret.Text = "Please Input A Command Else Type Help"
+ret.TextColor3 = Color3.new(1, 1, 1)
+ret.BackgroundTransparency = 1
+ret.Parent = UI
+ret.TextSize = 10
+
+local Input = Instance.new("TextBox")
+Input.Name = "Input"
+Input.Size = UDim2.new(0.2, 0, 0.05, 0)
+Input.Position = UDim2.new(0.12, 2, 0.08, 2)
+Input.ClearTextOnFocus = true
+Input.Parent = UI
+Input.TextColor3 = Color3.new(1, 1, 1)
+Input.Text = ""
+Input.BorderSizePixel = 0
+Input.BackgroundTransparency = 1
+Input.TextXAlignment = "Left"
+Input.TextSize = 15
+
+game.StarterGui:SetCore("SendNotification", {Title = "CMD Loaded", Text = "Have Fun", Duration = 4,})
+
+while wait() do
+    if i == true then
+        if Input.Text == "Close" then
+            wait(1)
+            UI:Destroy()
+            game.StarterGui:SetCore("SendNotification", {Title = "Thank You", Text = "Come Again", Duration = 4,})
+            Input.Text = ""
+            i = false
+        end
+
+        if Input.Text == "Light Mode" then
+            wait(1)
+            base.BackgroundColor3 = Color3.new(1, 1, 1)
+            Input.Text = ""
+        end
+
+        if Input.Text == "Dark Mode" then
+            wait(1)
+            base.BackgroundColor3 = Color3.new(0, 0, 0)
+            Input.Text = ""
+        end
+
+        if Input.Text == "cooolchill_X Hub" then
+            wait(1)
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/HUB.lua"))()
+            Input.Text = ""
+        end
+
+        if Input.Text == "Help" then
+            wait(1)
+            local list = Instance.new("TextLabel")
+            list.Name = "Command List"
+            list.Size = UDim2.new(0, 100, 0, 20)
+            list.Position = UDim2.new(0.5, 0, 0, 0)
+            list.Text = "Commands"
+            list.TextColor3 = Color3.new(1, 1, 1)
+            list.BackgroundTransparency = 1
+            list.TextSize = 10
+            list.Parent = UI
+
+            local list2 = Instance.new("TextLabel")
+            list2.Name = "Command 1"
+            list2.Size = UDim2.new(0, 100, 0, 20)
+            list2.Position = UDim2.new(0.5, 0, 0.05, 0)
+            list2.Text = "Close"
+            list2.TextColor3 = Color3.new(1, 1, 1)
+            list2.BackgroundTransparency = 1
+            list2.TextSize = 8
+            list2.Parent = UI
+
+            local list3 = Instance.new("TextLabel")
+            list3.Name = "Command 2"
+            list3.Size = UDim2.new(0, 100, 0, 20)
+            list3.Position = UDim2.new(0.5, 0, 0.1, 0)
+            list3.Text = "Light Mode"
+            list3.TextColor3 = Color3.new(1, 1, 1)
+            list3.BackgroundTransparency = 1
+            list3.TextSize = 8
+            list3.Parent = UI
+
+            local list4 = Instance.new("TextLabel")
+            list4.Name = "Command 3"
+            list4.Size = UDim2.new(0, 100, 0, 20)
+            list4.Position = UDim2.new(0.5, 0, 0.15, 0)
+            list4.Text = "Dark Mode"
+            list4.TextColor3 = Color3.new(1, 1, 1)
+            list4.BackgroundTransparency = 1
+            list4.TextSize = 8
+            list4.Parent = UI
+
+            local list5 = Instance.new("TextLabel")
+            list5.Name = "Command 4"
+            list5.Size = UDim2.new(0, 100, 0, 20)
+            list5.Position = UDim2.new(0.5, 0, 0.2, 0)
+            list5.Text = "cooolchill_X Hub"
+            list5.TextColor3 = Color3.new(1, 1, 1)
+            list5.BackgroundTransparency = 1
+            list5.TextSize = 8
+            list5.Parent = UI
+            Input.Text = ""
+        end
+
+        if Input.Text == "Light Mode" then
+            wait(1)
+            Input.TextColor3 = Color3.new(0, 0, 0)
+            ret.TextColor3 = Color3.new(0, 0, 0)
+            label.TextColor3 = Color3.new(0, 0, 0)
+            arrow.TextColor3 = Color3.new(0, 0, 0)
+            base.BackgroundColor3 = Color3.new(1, 1, 1)
+            help.BackgroundColor3 = Color3.new(1, 1, 1)
+            Input.Text = ""
+        end
+
+        if Input.Text == "Dark Mode" then
+            wait(1)
+            Input.TextColor3 = Color3.new(1, 1, 1)
+            ret.TextColor3 = Color3.new(1, 1, 1)
+            label.TextColor3 = Color3.new(1, 1, 1)
+            arrow.TextColor3 = Color3.new(1, 1, 1)
+            base.BackgroundColor3 = Color3.new(0, 0, 0)
+            help.BackgroundColor3 = Color3.new(0, 0, 0)
+            Input.Text = ""
+        end
+    elseif i == false then
+        break
+    end
+end
