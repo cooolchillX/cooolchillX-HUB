4965a504c47116f69f96a4cda7648a6fc61f1541
cooolchillX <158112913+cooolchillX@users.noreply.github.com>
2026-08-28 17:49:22 -0400

    Update Hub Loadstring.lua

--- Hub Loadstring.lua
+++ Hub Loadstring.lua
@@ -1,3 +1,2 @@
 _G.autodetect = false -- If You Want To Auto Detect The Game
-_G.LowLevelExecutor = false -- If You Use Xeno Or Solara This Fixes The Script Hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/HUB.lua"))()
